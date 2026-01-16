811e45a547bcf98e5a282fdf235eb3c9908dc1f8
SalmanAljnidi <s6166s@yahoo.com>
2026-01-16 19:22:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,15 @@
-const CACHE_NAME = 'millionaire-math-v3';
-const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    './images/logo.png',
-    './images/icon-192.png',
-    './sounds/intro.mp3',
-    './sounds/correct.mp3',
-    './sounds/wrong.mp3',
-    './sounds/wait.mp3'
-];
-
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+  e.waitUntil(
+    caches.open('math-game-store').then((cache) => cache.addAll([
+      'index.html',
+      'style.css',
+      'script.js',
+    ])),
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
 });
